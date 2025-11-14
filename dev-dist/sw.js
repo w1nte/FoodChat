fc91d75cd179755247c9983dcd63e377354a325b
winte <hello@winte.xyz>
2025-11-14 19:43:20 +0100

    Fix styling

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gkaahi7vrf"
+    "revision": "0.1ptnlvk523"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
